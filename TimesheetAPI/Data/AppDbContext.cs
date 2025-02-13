b37675fd2998d461161ec27797520230ee4bcbe6
vitmonjo <vitmonjo@gmail.com>
2025-02-12 21:25:14 -0300

    Add testing with EF Core and Swagger

---
+++ TimesheetAPI/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿namespace TimesheetAPI.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using TimesheetAPI.Models;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<TimesheetEntry> TimesheetEntries { get; set; }
+        public DbSet<UserClient> UserClients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserClient>()
+                .HasKey(uc => new { uc.UserId, uc.ClientId }); // Composite PK
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+
+
+}
--- TimesheetAPI/Program.cs
+++ TimesheetAPI/Program.cs
@@ -1,11 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using TimesheetAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register DbContext with InMemoryDatabase
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("TimesheetDB"));
 
 var app = builder.Build();
 
@@ -17,9 +23,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();

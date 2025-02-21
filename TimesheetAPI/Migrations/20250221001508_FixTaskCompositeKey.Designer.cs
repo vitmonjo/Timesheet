c5b3dcda2a50d174271ed497919a0be662fa43d1
vitmonjo <vitmonjo@gmail.com>
2025-02-20 23:51:46 -0300

    Apply migrations

--- TimesheetAPI/Migrations/20250221001508_FixTaskCompositeKey.Designer.cs
+++ TimesheetAPI/Migrations/20250221001508_FixTaskCompositeKey.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TimesheetAPI.Data;
 
@@ -11,9 +12,11 @@ using TimesheetAPI.Data;
 namespace TimesheetAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250221001508_FixTaskCompositeKey")]
+    partial class FixTaskCompositeKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

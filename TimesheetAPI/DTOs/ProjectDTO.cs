6dac3a36be7bc94a9b59a7a3a77ac7dd95d42e0a
vitmonjo <vitmonjo@gmail.com>
2025-03-03 10:11:28 -0300

    Create DTO for Project

---
+++ TimesheetAPI/DTOs/ProjectDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TimesheetAPI.DTOs
+{
+    public class ProjectDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int ClientId { get; set; }  // Foreign Key
+    }
+}

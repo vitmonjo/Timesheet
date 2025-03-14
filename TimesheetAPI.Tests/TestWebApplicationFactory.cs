f789dcb7513741f11ab5cfee852912eca0e3284e
vitmonjo <vitmonjo@gmail.com>
2025-03-14 18:12:18 -0300

    Remove TestWebApplicationFactory

--- TimesheetAPI.Tests/TestWebApplicationFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
-
-namespace TimesheetAPI.Tests;
-
-public class TestWebApplicationFactory : WebApplicationFactory<Program>
-{
-}

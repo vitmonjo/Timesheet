f4fe5e6a54c78cb1ad4a8a231dfe16c61691d72b
vitmonjo <vitmonjo@gmail.com>
2025-03-14 18:15:22 -0300

    Recreates TestWebApplicationFactory

---
+++ TimesheetAPI.Tests/IntegrationTests/TestWebApplicationFactory.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace TimesheetAPI.Tests;
+
+public class TestWebApplicationFactory : WebApplicationFactory<Program>
+{
+}

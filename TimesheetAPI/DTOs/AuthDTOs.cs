ec274af04c9e733019cf2c77f62cdbb548e4b1bd
vitmonjo <vitmonjo@gmail.com>
2025-03-26 23:27:36 -0300

    Create AuthDTO

---
+++ TimesheetAPI/DTOs/AuthDTOs.cs
@@ -0,0 +1,19 @@
+﻿namespace TimesheetAPI.DTOs
+{
+    public class RegisterDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class  LoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class AuthResponseDTO
+    {
+        public string Token { get; set; }
+    }
+}

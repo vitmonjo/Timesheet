f466ef7a2c803ad899b1fdebe5967161328087fe
vitmonjo <vitmonjo@gmail.com>
2025-04-02 20:57:58 -0300

    Create AuthController

---
+++ TimesheetAPI/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TimesheetAPI.Authentication;
+using TimesheetAPI.DTOs;
+using TimesheetAPI.Authentication;
+
+namespace TimesheetAPI.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterDTO registerDto)
+        {
+            try
+            {
+                var response = _authService.Register(registerDto);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginDTO loginDto)
+        {
+            try
+            {
+                var response = _authService.Login(loginDto);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+    }
+}
